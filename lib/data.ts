aa7ee2ec282672196c1f35f8ace446d12d9b6a51
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-04-30 18:42:19 +0500

    data changes

--- lib/data.ts
+++ lib/data.ts
@@ -1,34 +1,34 @@
-// import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 
-// const data = {
-//   users: [
-//     {
-//       name: 'Admin',
-//       email: 'admin@example.com',
-//       password: bcrypt.hashSync('admin001'),
-//       isAdmin: true,
-//     },
-//     {
-//       name: 'User',
-//       email: 'user@example.com',
-//       password: bcrypt.hashSync('12345'),
-//       isAdmin: false,
-//     },
-//   ],
-//   products: [
-//     {
-//       name: 'Coatings',
-//       slug: 'coatings-1',
-//       category: 'Paints',
-//       image: '/images/paint-1.png',
-//       price: 15,
-//       brand: 'Coatings',
-//       rating: 2.4,
-//       numReviews: 14,
-//       countInStock: 20,
-//       description: 'A popular paints',
-//     },
-//   ],
-// }
+const data = {
+  users: [
+    {
+      name: 'Admin',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('admin001'),
+      isAdmin: true,
+    },
+    {
+      name: 'User',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('12345'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: 'Coatings',
+      slug: 'coatings-1',
+      category: 'Category',
+      image: '/images/paint-1.png',
+      price: 15,
+      brand: 'Coatings',
+      rating: 2.4,
+      numReviews: 14,
+      countInStock: 20,
+      description: 'A popular paints',
+    },
+  ],
+}
 
-// export default data
+export default data
